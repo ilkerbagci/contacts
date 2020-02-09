89e92a94575ba42ae0eb0a5a46b3084e38582382
İlker Bağcı <ilkerbagci0@gmail.com>
2020-02-09 05:12:40 +0300

    Add CreateContact component and state for screen content

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import ListContacts from './ListContacts'
 import * as ContactsAPI from './utils/ContactsAPI'
- 
+import CreateContact from './CreateContact'
+
 class App extends Component {
   state = {
-    contacts: []
+    contacts: [],
+    screen : 'create'
   }
   componentDidMount() {
     ContactsAPI.getAll()
@@ -23,16 +25,20 @@ class App extends Component {
     })
     )
     ContactsAPI.remove(contact)
-
   }
 
   render() {
     return (
       <div>
-        <ListContacts 
-          contacts={this.state.contacts} 
-          onDeleteContact={this.removeContact}
-        />
+        {this.state.screen === 'list' && (
+          <ListContacts 
+            contacts={this.state.contacts} 
+            onDeleteContact={this.removeContact}
+          />
+        )}
+        {this.state.screen === 'create' && (
+          <CreateContact/>)
+        }
       </div>
     );
   }
---
+++ src/CreateContact.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class CreateContact extends Component{
+
+    render(){
+        return(
+            <div>
+                Create Contact Component
+            </div>
+        )
+    }
+}
+
+export default CreateContact;
